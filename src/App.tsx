918148a1fdd361a1001f202f09a7f7ea8f28ac49
brob1115 <broberts111592@gmail.com>
2023-08-24 13:04:49 -0500

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
 				</a>
 			</div>
 			<h1>Lil Sloot</h1>
+			<h1>Fckn thot whore bitch slut skank</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
