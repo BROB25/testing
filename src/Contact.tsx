87fd9044c509ee7b6994b7ffb20bd05180570d27
brob1115 <broberts111592@gmail.com>
2023-08-24 20:39:56 -0500

    idk

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -14,7 +14,7 @@ const Contact = () => {
 	return (
 		<section id="contact" className="mx-auto pb-12">
 			<h2 className="flex justify-center text-3xl sm:text-4xl font-bold text-gray-400 mt-12 mb-4 slide-right">
-				Contact Me!
+				HOE ASS NIGGA!!!
 			</h2>
 			<div className="grid grid-cols-12 justify-center">
 				<div className="col-span-12 md:col-start-3 md:col-span-8 px-4">
