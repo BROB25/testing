6155356d78eb10011eb88546fb21a95393499b3b
brob1115 <broberts111592@gmail.com>
2023-09-13 13:51:11 -0500

    pricing menu

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -19,9 +19,21 @@ const Menu = () => {
 				Pricing <BiSolidChevronDown size={23} className="pl-2" />
 			</Button>
 			{isOpen && (
-				<div className="menu absolute top-full mt-2 w-[300px] h-[200px] p-4 bg-black border border-4 border-gray-400 rounded-xl shadow-lg z-30">
-					{/* Add your menu content here */}
-					Menu Content
+				<div className="menu absolute top-full mt-2 w-[300px] h-[200px] p-4 bg-black border border-4 border-gray-400 rounded-xl shadow-lg z-90">
+					<div className="flex flex-col justify-start">
+						<div className="mb-2">
+							<p className="text-white text-left font-bold">
+								Prices as low as:
+							</p>
+							<h3 className="text-green-600 text-lg font-extrabold"> $80</h3>
+						</div>
+						<div>
+							<p className="text-white text-left font-bold">
+								Game attachments:
+							</p>
+							<h3 className="text-green-600 text-lg font-extrabold"> $20</h3>
+						</div>
+					</div>
 				</div>
 			)}
 		</div>
